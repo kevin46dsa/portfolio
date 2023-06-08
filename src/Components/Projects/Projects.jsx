0b538672b03fd0df1557c9231317bd5de39e9850
kevin46dsa <kevin0108dsa@gmail.com>
2023-06-08 16:35:06 -0400

    scrolling

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import { octokit } from '../../octokit';
-
+import './Projects.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from "react-bootstrap/esm/Col";
 import { Card, Carousel,Button,Modal } from 'react-bootstrap';
+
 
 
 const Projects = () => {
@@ -36,15 +37,16 @@ onLoad();
 
 
   return (<div>
-        <h1>My Projects</h1>
+        <h1 style={{textAlign:"center"}}>My Projects</h1>
         <br/>
         <Container className=''>
           <Row>
             <Col>
-            <Card>
+            <Card >
             <Carousel >
             <Carousel.Item>
                 <img
+                style={{ height: "300px" }}
                 className="d-block w-100"
                 src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
                 alt="First slide"/>
@@ -71,8 +73,40 @@ onLoad();
         Read More
       </Button>
       </Card.Body>
-                </Card>
+      </Card>
             </Col>
+            <Col>
+            <Card>
+        <Card.Body>
+        <div class="scroll">
+  
+        # Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+  </div>
+        </Card.Body>
+      </Card>
+      </Col>
           </Row>
         </Container>
         <Modal show={show} onHide={handleClose}>
