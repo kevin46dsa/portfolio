e2e152f1c8b12a4f79921fe97080e1ea3a508c10
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2023-12-02 20:08:48 -0500

    tool tip on about page

--- src/Components/About/Techstack.jsx
+++ src/Components/About/Techstack.jsx
@@ -15,39 +15,151 @@ import {
   SiNextdotjs,
 } from "react-icons/si";
 import './MyCard.css';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} >
-        <Card className="my-card"><CgCPlusPlus size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              C++
+            </Tooltip>
+          }
+        >
+         <Card className="my-card"><CgCPlusPlus size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><DiJavascript1 size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              JavaScript
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><DiJavascript1 size={40}/></Card>
+        </OverlayTrigger>
+       
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><DiNodejs size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Node JS
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><DiNodejs size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><DiReact size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              React JS
+            </Tooltip>
+          }
+        >
+         <Card className="my-card"><DiReact size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><DiMongodb size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              MongoDB
+            </Tooltip>
+          }
+        >
+           <Card className="my-card"><DiMongodb size={40}/></Card>
+        </OverlayTrigger>
+       
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><SiNextdotjs size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              NextJS
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><SiNextdotjs size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><DiGit size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Git
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><DiGit size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><SiFirebase size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              FireBase
+            </Tooltip>
+          }
+        >
+           <Card className="my-card"><SiFirebase size={40}/></Card>
+        </OverlayTrigger>
+       
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><DiPython size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Python
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><DiPython size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2} >
-        <Card className="my-card"><SiPytorch size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              PyTorch
+            </Tooltip>
+          }
+        >
+           <Card className="my-card"><SiPytorch size={40}/></Card>
+        </OverlayTrigger>
+       
       </Col>
     </Row>
   );
--- src/Components/About/Toolstack.jsx
+++ src/Components/About/Toolstack.jsx
@@ -6,27 +6,101 @@ import {
   SiPostman,
   SiHeroku,
   SiVercel,
+  SiNetlify
 } from "react-icons/si";
 import './MyCard.css';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';;
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2}>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+            Linux
+            </Tooltip>
+          }
+        >
         <Card className="my-card"><SiLinux size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><SiVisualstudiocode size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Visual Studio Code
+            </Tooltip>
+          }
+        >
+               <Card className="my-card"><SiVisualstudiocode size={40}/></Card>
+        </OverlayTrigger>
+
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><SiPostman size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Postman
+            </Tooltip>
+          }
+        >
+          <Card className="my-card"><SiPostman size={40}/></Card>
+        </OverlayTrigger>
+       
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><SiVercel size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+             Vercel
+            </Tooltip>
+          }
+        >
+         <Card className="my-card"><SiVercel size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
       <Col xs={4} md={2}>
-        <Card className="my-card"><SiHeroku size={40}/></Card>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Heroku
+            </Tooltip>
+          }
+        >
+         <Card className="my-card"><SiHeroku size={40}/></Card>
+        </OverlayTrigger>
+        
       </Col>
+      <Col xs={4} md={2}>
+      <OverlayTrigger
+          key='top'
+          placement='top'
+          overlay={
+            <Tooltip id={`tooltip-top`}>
+              Netlify
+            </Tooltip>
+          }
+        >
+         <Card className="my-card"><SiNetlify size={40}/></Card>
+        </OverlayTrigger>
+        
+      </Col>
+      
     </Row>
   );
 }
