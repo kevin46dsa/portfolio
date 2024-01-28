72ebdda18b922023d3768eab7e9ca5d59103cbc3
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2024-01-28 15:21:50 -0500

    Updated Resume

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-
+// Document taken from Google Drive kevin0108dsa@gmail.com > Resume for Portfolio Website
 const Resume = () => {
   return (
   <div style={{height:"100vh"}}>
-    <iframe title='Resume' src="https://drive.google.com/file/d/1wAEi3wNi_l2ihWHOdLj3Hp_JPdsDn7XS/preview" width="100%" height="95%" ></iframe>
+    <iframe title='Resume' src="https://drive.google.com/file/d/1bAlAsGp04gEccT6UKey9IXV5cHOBaJxZ/preview" width="100%" height="95%" ></iframe>
   </div>
   );
 };
+
 
 export default Resume;
