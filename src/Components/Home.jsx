0f3c51783e188bc3ea66ec596f54996014e2d503
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2024-01-28 16:53:53 -0500

    fixed responsiveness of the main page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,12 +6,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image'
 import "./Home.css"
-import kevimg from "../Assets/1664229246147.jpeg"
+import kevimg from "../Assets/profilephoto.png"
 import QRcode from "../Assets/kevindsa.png"
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 import Stack from 'react-bootstrap/Stack';
+//<Image src={kevimg} style={{height:"500px"}}  />
 
 const Home = () => {
   return (
@@ -21,21 +22,25 @@ const Home = () => {
         <br/>
         <Container fluid>
         <Row>
-          <Col sm="12" md="12" lg="7" style={{textAlign: "center"}}>
+          <Col lg="1"></Col>
+          <Col sm="12" md="12" lg="5" className='d-flex  justify-content-center flex-column align-items-center'>
             <div >
-            <Image src={kevimg} style={{height:"500px"}} className="shadow-lg p-3 mb-5 bg-white rounded" />
+            <Image src={kevimg} className="img-fluid" style={{ maxHeight: "525px"}} alt="Responsive Image" />
             </div>
           </Col>
-          <Col sm="12" md="12" lg="5" >
-          <div>
+          <Col sm="12" md="12" lg="5" className='d-flex  justify-content-center flex-column align-items-center'>
+          
+          <div >
+          <br/>
           <h1>Hi, I am Kevin D'sa !!</h1>
           <h2>Welcome to my Website</h2>
           <br/>
           </div>
           
           <div className='qrCode'>
-          <Image src={QRcode} style={{height:"375px"}} className="shadow-lg p-3 mb-5 bg-white rounded"/>
+          <Image src={QRcode} className="img-fluid bg-white rounded" style={{ maxHeight: "350px" }} alt="Responsive Image" />
           </div>
+          <br/>
           <div >
           <Stack direction="horizontal" gap={3}>
       
@@ -60,7 +65,9 @@ const Home = () => {
     <br/>
         </div>
       </Col>
+      <Col lg="1"></Col>
         </Row>
+       
         </Container>
        
   </div>
