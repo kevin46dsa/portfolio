cb60896955c1307f2c5307f14991766f8723259a
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2024-12-31 01:10:09 +0530

    Add Footer component and update devDependencies in package.json

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div>
+      <div style={{ backgroundColor: "black" }}>
+        <h1>Hello i am a footer</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
