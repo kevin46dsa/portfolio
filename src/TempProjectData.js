5e7fa8135859d9948184827ca639a9b8fce3a08b
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2025-05-25 00:45:32 -0400

    fixed project image size

--- src/TempProjectData.js
+++ src/TempProjectData.js
@@ -1,52 +1,66 @@
 export const projectData = [
-      {
-        projectName:"SoulMate",
-        projectDescription:"This a Project primaryly focus on solar project management system that we prepared for CS-555-Agile Development Project using React, Firebase  Team: Easy Scurm Team Members: Rongda Kang,Kevin Dsa, Yuzhi Wang(Scrum Master),Luoyi Fu, Mingze Sun.",
-        slide1:"https://firebasestorage.googleapis.com/v0/b/agile-11fe1.appspot.com/o/Screenshot%202024-01-07%20at%206.51.08%E2%80%AFPM.png?alt=media&token=98482463-495a-4bae-9bcb-bc79e5c3aaf0",
-        slide2:"https://firebasestorage.googleapis.com/v0/b/agile-11fe1.appspot.com/o/Screenshot%202024-01-07%20at%206.52.10%E2%80%AFPM.png?alt=media&token=e0d69946-a2af-4968-aad1-63cf64d954ca",
-        color:"#FDF5E6",
-        githubLink:"https://github.com/kevin46dsa/CS-555-project",
-        websitePage:"https://soulmatecs555.netlify.app/"
-      },
-     {
-      projectName:"RentPipe",
-      projectDescription:"Modern UI/UX website using React.js & ReactBoostrap Final Project for Course CS 545 Human Computer Interaction at Stevens Institute of technology",
-      thumbnail:"https://i.ibb.co/wLv47Wm/Rent-Pipe-logos-white-adobe-express.png",
-      slide1:"https://firebasestorage.googleapis.com/v0/b/portfolio-235df.appspot.com/o/Screenshot%202023-07-09%20at%206.25.54%20PM.png?alt=media&token=113a4996-bd1a-47cc-b9c7-c75160814d05",
-      slide2:"https://firebasestorage.googleapis.com/v0/b/portfolio-235df.appspot.com/o/Screenshot%202023-07-09%20at%206.26.35%20PM.png?alt=media&token=2e7a7903-cd2c-4184-bc7f-ac50507e3805",
-      color:"#F0F8FF",
-      githubLink:"https://github.com/kevin46dsa/RentPipe",
-      websitePage:"https://rentpipe.netlify.app/"
-     },
-     {
-      projectName:"3D T-Shirt Website using React + Three.JS",
-      projectDescription:"Reactive 3D T-Shirt Customizer Web Application leveraging the power of Three.JS Framer Motion and 3D TShirt Model. Inspired by youtube channel Javascript Mastery",
-      slide1:"https://github.com/kevin46dsa/3D-Website-shop/assets/67777018/80b9dae2-49a1-42b8-97f9-864051e6d811",
-      slide2:"https://github.com/kevin46dsa/3D-Website-shop/assets/67777018/eab416d5-08f8-4396-aa7d-811b66c2bd55",
-      color:"#FDF5E6",
-      githubLink:"https://github.com/kevin46dsa/3D-Website-shop",
-      websitePage:"https://3dtshirtcustomizerjsm.netlify.app/"
-    },
-     {
-      projectName:"Budget Expense Tracker",
-      projectDescription:"MERN Fullstack website, Final Project for Course CS 546 Web Programming at Stevens Institute of technology",
-      thumbnail:"https://i.ibb.co/wLv47Wm/Rent-Pipe-logos-white-adobe-express.png",
-      slide1:"https://user-images.githubusercontent.com/67777018/189949158-ed97b4a3-d114-4772-ba36-4244bd721d6e.PNG",
-      slide2:"https://user-images.githubusercontent.com/67777018/189949160-3765802d-0a40-4d65-ae13-fe90288081cd.PNG",
-      color:"#FDF5E6",
-      githubLink:"https://github.com/kevin46dsa/BET-Client",
-      websitePage:"https://betclient.herokuapp.com/"
-    },
-    
-    {
-      projectName:"NBA Career Longevity Prediction",
-      projectDescription:"This repository contains the code for the NBA Career Longevity Prediction project developed for the CS 513 course. The project aims to predict the career longevity of NBA players based on various features and factors.",
-    
-      slide1:"https://user-images.githubusercontent.com/67777018/245667029-8527140e-1bfa-498e-9972-933a5b752225.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ2NzA0ODAsIm5iZiI6MTcwNDY3MDE4MCwicGF0aCI6Ii82Nzc3NzAxOC8yNDU2NjcwMjktODUyNzE0MGUtMWJmYS00OThlLTk5NzItOTMzYTViNzUyMjI1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA3VDIzMjk0MFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJlZmQ2YzRjNGEzNTNiMjg0NmE1NjdiNjliN2I0ZGM2ODg2YWMyYzcxYTI1OGRiOTFmNjdlMGNlNzI1MzE2MWMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.3rbuaSOENhiKNmjPy-Rnr85t8em_hVgQgt6Jvy7noXc",
-      slide2:"https://user-images.githubusercontent.com/67777018/245667040-139c3a3c-256a-402f-943d-146e57635157.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY0NzQyODUsIm5iZiI6MTcwNjQ3Mzk4NSwicGF0aCI6Ii82Nzc3NzAxOC8yNDU2NjcwNDAtMTM5YzNhM2MtMjU2YS00MDJmLTk0M2QtMTQ2ZTU3NjM1MTU3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTI4VDIwMzMwNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTY3MjAwZDIzNjhhZThjZmQ0YjVmZDUwNTFhMjBiNTY3NDg1ZTZlNjYyZDIyYjg3MmEzNTYxYTBlZmEzNDEwYmYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.6pKLiFNOX5TH52py7MWvbhsbGNw8RQEYV4--_Ky1MNg",
-      color:"#FDF5E6",
-      githubLink:"https://github.com/kevin46dsa/NBA-Career-Longevity-Pridiction",
-      websitePage:""
-    },
-  
-  ]
+  {
+    projectName: "SoulMate",
+    projectDescription:
+      "This a Project primaryly focus on solar project management system that we prepared for CS-555-Agile Development Project using React, Firebase  Team: Easy Scurm Team Members: Rongda Kang,Kevin Dsa, Yuzhi Wang(Scrum Master),Luoyi Fu, Mingze Sun.",
+    slides: [
+      "https://firebasestorage.googleapis.com/v0/b/agile-11fe1.appspot.com/o/Screenshot%202024-01-07%20at%206.51.08%E2%80%AFPM.png?alt=media&token=98482463-495a-4bae-9bcb-bc79e5c3aaf0",
+      "https://firebasestorage.googleapis.com/v0/b/agile-11fe1.appspot.com/o/Screenshot%202024-01-07%20at%206.52.10%E2%80%AFPM.png?alt=media&token=e0d69946-a2af-4968-aad1-63cf64d954ca",
+    ],
+    color: "#FDF5E6",
+    githubLink: "https://github.com/kevin46dsa/CS-555-project",
+    websitePage: "https://soulmatecs555.netlify.app/",
+  },
+  {
+    projectName: "RentPipe",
+    projectDescription:
+      "Modern UI/UX website using React.js & ReactBoostrap Final Project for Course CS 545 Human Computer Interaction at Stevens Institute of technology",
+    thumbnail:
+      "https://i.ibb.co/wLv47Wm/Rent-Pipe-logos-white-adobe-express.png",
+    slides: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-235df.appspot.com/o/Screenshot%202023-07-09%20at%206.25.54%20PM.png?alt=media&token=113a4996-bd1a-47cc-b9c7-c75160814d05",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-235df.appspot.com/o/Screenshot%202023-07-09%20at%206.26.35%20PM.png?alt=media&token=2e7a7903-cd2c-4184-bc7f-ac50507e3805",
+    ],
+    color: "#F0F8FF",
+    githubLink: "https://github.com/kevin46dsa/RentPipe",
+    websitePage: "https://rentpipe.netlify.app/",
+  },
+  {
+    projectName: "3D T-Shirt Website using React + Three.JS",
+    projectDescription:
+      "Reactive 3D T-Shirt Customizer Web Application leveraging the power of Three.JS Framer Motion and 3D TShirt Model. Inspired by youtube channel Javascript Mastery",
+    slides: [
+      "https://github.com/kevin46dsa/3D-Website-shop/assets/67777018/80b9dae2-49a1-42b8-97f9-864051e6d811",
+      "https://github.com/kevin46dsa/3D-Website-shop/assets/67777018/eab416d5-08f8-4396-aa7d-811b66c2bd55",
+    ],
+    color: "#FDF5E6",
+    githubLink: "https://github.com/kevin46dsa/3D-Website-shop",
+    websitePage: "https://3dtshirtcustomizerjsm.netlify.app/",
+  },
+  {
+    projectName: "Budget Expense Tracker",
+    projectDescription:
+      "MERN Fullstack website, Final Project for Course CS 546 Web Programming at Stevens Institute of technology",
+    thumbnail:
+      "https://i.ibb.co/wLv47Wm/Rent-Pipe-logos-white-adobe-express.png",
+    slides: [
+      "https://user-images.githubusercontent.com/67777018/189949158-ed97b4a3-d114-4772-ba36-4244bd721d6e.PNG",
+      "https://user-images.githubusercontent.com/67777018/189949160-3765802d-0a40-4d65-ae13-fe90288081cd.PNG",
+    ],
+    color: "#FDF5E6",
+    githubLink: "https://github.com/kevin46dsa/BET-Client",
+    websitePage: "https://betclient.herokuapp.com/",
+  },
+  {
+    projectName: "NBA Career Longevity Prediction",
+    projectDescription:
+      "This repository contains the code for the NBA Career Longevity Prediction project developed for the CS 513 course. The project aims to predict the career longevity of NBA players based on various features and factors.",
+    slides: [
+      "https://user-images.githubusercontent.com/67777018/245667029-8527140e-1bfa-498e-9972-933a5b752225.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ2NzA0ODAsIm5iZiI6MTcwNDY3MDE4MCwicGF0aCI6Ii82Nzc3NzAxOC8yNDU2NjcwMjktODUyNzE0MGUtMWJmYS00OThlLTk5NzItOTMzYTViNzUyMjI1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA3VDIzMjk0MFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJlZmQ2YzRjNGEzNTNiMjg0NmE1NjdiNjliN2I0ZGM2ODg2YWMyYzcxYTI1OGRiOTFmNjdlMGNlNzI1MzE2MWMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.3rbuaSOENhiKNmjPy-Rnr85t8em_hVgQgt6Jvy7noXc",
+      "https://user-images.githubusercontent.com/67777018/245667040-139c3a3c-256a-402f-943d-146e57635157.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY0NzQyODUsIm5iZiI6MTcwNjQ3Mzk4NSwicGF0aCI6Ii82Nzc3NzAxOC8yNDU2NjcwNDAtMTM5YzNhM2MtMjU2YS00MDJmLTk0M2QtMTQ2ZTU3NjM1MTU3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTI4VDIwMzMwNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTY3MjAwZDIzNjhhZThjZmQ0YjVmZDUwNTFhMjBiNTY3NDg1ZTZlNjYyZDIyYjg3MmEzNTYxYTBlZmEzNDEwYmYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.6pKLiFNOX5TH52py7MWvbhsbGNw8RQEYV4--_Ky1MNg",
+    ],
+    color: "#FDF5E6",
+    githubLink: "https://github.com/kevin46dsa/NBA-Career-Longevity-Pridiction",
+    websitePage: "",
+  },
+];
